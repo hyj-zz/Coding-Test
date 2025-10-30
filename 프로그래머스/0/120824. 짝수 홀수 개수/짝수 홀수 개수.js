779a269fd98ffc7870df8fe4b9b70b4c16c0af7f
hyj-zz <133831101+hyj-zz@users.noreply.github.com>
2025-10-30 17:53:12 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.js
+++ 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.js
@@ -1,7 +1,5 @@
 function solution(num_list) {
     let answer = [0, 0]
-    num_list.map((e) => {
-        e % 2 == 0 ? answer[0]++ : answer[1]++ ;
-    })
+    num_list.forEach(e => e % 2 == 0 ? answer[0]++ : answer[1]++);
     return answer;
 }
